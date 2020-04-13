9af2b711f0204a95fbd3cbbb17b3444cc5e3eb6f
panwang <ink_pan@hotmail.com>
2020-04-13 12:48:01 +0800

    Restructured the file. Debuged get() in DataLoader(). Successfully using train(). Add load() save() module

---
+++ DataLoader.py
@@ -0,0 +1,124 @@
+# -*- coding:utf-8 -*-
+# Created at 2020-04-13
+# Filename:DataLoader.py
+# Author:Wang Pan
+# Purpose:
+import pandas as pd
+import pickle
+import networkx as nx
+import sys
+import scipy
+import numpy as np
+
+class DataLoader():
+    def __init__(self,args):
+        #initialization
+        data_dir=args['data_dir']
+        df=pd.read_hdf(data_dir)
+        df_length=len(df)
+        adj_mx_dir=args['adj_mx_dir']
+        with open(adj_mx_dir,'rb') as f:
+            self.adj_mx=pickle.load(f,encoding='latin1')[2]
+        train_ratio=args['train_ratio']
+        test_ratio=args['test_ratio']
+        val_ratio=args['val_ratio']
+        self.seq_len=args['seq_len']
+        self.horizon=args['horizon']
+        assert (train_ratio+val_ratio+test_ratio==1)
+
+        df_set={'train':df[:int(train_ratio*df_length)],
+                'val':df[int(train_ratio*df_length):int((train_ratio+val_ratio)*df_length)],
+                'test':df[-int(test_ratio*df_length):]}
+
+        #Construct Graph
+        graph = self.mat_to_nx(self.adj_mx)
+        n = graph.number_of_nodes()
+        m = graph.number_of_edges()
+        print('Graph have %d nodes and %d links.\n'
+              'Input sequence length: %d \n'
+              'Forecasting horizon: %d \n'
+               % (
+                  n, m, self.seq_len, self.horizon),
+              file=sys.stderr)
+        self.graph = graph
+        self.data = {}
+        for each in df_set:
+            xy={}
+            xy['x'],xy['y']=self.construct_x_y(df_set[each])
+            xy['y']=xy['y'][...,[0]]
+            self.data[each]=xy
+        self.stage=None
+
+
+    @staticmethod
+    def mat_to_nx(adj_mat):
+        g = nx.Graph()
+        g.add_nodes_from(range(adj_mat.shape[0]))
+        coo = scipy.sparse.coo_matrix(adj_mat)
+        for u, v, _ in zip(coo.row, coo.col, coo.data):
+            g.add_edge(u, v)
+        assert g.number_of_nodes() == adj_mat.shape[0]
+        return g
+
+    def construct_x_y(self,
+            df, add_time_in_day=True, add_day_in_week=False
+    ):
+        """
+        Generate samples from
+        :param df:
+        :param add_time_in_day:
+        :param add_day_in_week:
+        :return:
+        # x: (epoch_size, seq_len, num_nodes, input_dim)
+        # y: (epoch_size, horizon, num_nodes, output_dim)
+        """
+
+        num_samples, num_nodes = df.shape
+        data = np.expand_dims(df.values, axis=-1)
+        data_list = [data]
+
+        if add_time_in_day:
+            time_ind = (df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
+            time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
+            data_list.append(time_in_day)
+        if add_day_in_week:
+            day_in_week = np.zeros(shape=(num_samples, num_nodes, 7))
+            day_in_week[np.arange(num_samples), :, df.index.dayofweek] = 1
+            data_list.append(day_in_week)
+
+        x_offsets=np.arange(-self.seq_len+1,1)
+        y_offsets=np.arange(1,self.horizon+1)
+        data = np.concatenate(data_list, axis=-1)
+        # epoch_len = num_samples + min(x_offsets) - max(y_offsets)
+        x, y = [], []
+        # t is the index of the last observation.
+        min_t = abs(min(x_offsets))
+        max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
+        for t in range(min_t, max_t):
+            x_t = data[t + x_offsets, ...]
+            y_t = data[t + y_offsets, ...]
+            x.append(x_t)
+            y.append(y_t)
+        x = np.stack(x, axis=0)
+        y = np.stack(y, axis=0)
+        return x, y
+
+    def set(self,stage):
+        self.stage=stage
+        print("Now using %sing set"%(stage))
+        return stage
+
+    def get(self,batch_size):
+        self.current_batch=0
+        data=self.data[self.stage]
+        length=len(data['x'])
+        batches=length/batch_size
+        def iterator():
+            while self.current_batch<batches:
+                idx=self.current_batch*batch_size
+                yield(data['x'][idx:idx+batch_size],data['y'][idx:idx+batch_size])
+                self.current_batch=self.current_batch+1
+        return iterator()
+
+
+
--- main.py
+++ main.py
@@ -3,9 +3,8 @@
 # Author:Wang Pan
 # Purpose:
 #
-import numpy as np
-import pandas as pd
 import pickle
+from DataLoader import DataLoader
 import torch
 import torch.autograd
 import torch.nn as nn
@@ -13,150 +12,80 @@ import torch.nn.functional as fn
 import torch.optim as optim
 import argparse
 import yaml
-import networkx as nx
-import sys
-import scipy
 
 
-class DataLoader():
-    def __init__(self,args):
-        #initialization
-        data_dir=args['data_dir']
-        df=pd.read_hdf(data_dir)
-        df_length=len(df)
-        adj_mx_dir=args['adj_mx_dir']
-        with open(adj_mx_dir,'rb') as f:
-            self.adj_mx=pickle.load(f,encoding='latin1')[2]
-        train_ratio=args['train_ratio']
-        test_ratio=args['test_ratio']
-        val_ratio=args['val_ratio']
-        self.seq_len=args['seq_len']
-        self.horizon=args['horizon']
-        assert (train_ratio+val_ratio+test_ratio==1)
-
-        df_set={'train':df[:int(train_ratio*df_length)],
-                'val':df[int(train_ratio*df_length):int((train_ratio+val_ratio)*df_length)],
-                'test':df[-int(test_ratio*df_length):]}
-
-        #Construct Graph
-        graph = self.mat_to_nx(self.adj_mx)
-        n = graph.number_of_nodes()
-        m = graph.number_of_edges()
-        print('Graph have %d nodes and %d links.\n'
-              'Input sequence length: %d \n'
-              'Forecasting horizon: %d \n'
-               % (
-                  n, m, self.seq_len, self.horizon),
-              file=sys.stderr)
-        self.graph = graph
-        self.data = {}
-        for each in df_set:
-            xy={}
-            xy['x'],xy['y']=self.construct_x_y(df_set[each])
-            xy['y']=xy['y'][...,[0]]
-            self.data[each]=xy
-        self.stage=None
-
-
-    @staticmethod
-    def mat_to_nx(adj_mat):
-        g = nx.Graph()
-        g.add_nodes_from(range(adj_mat.shape[0]))
-        coo = scipy.sparse.coo_matrix(adj_mat)
-        for u, v, _ in zip(coo.row, coo.col, coo.data):
-            g.add_edge(u, v)
-        assert g.number_of_nodes() == adj_mat.shape[0]
-        return g
-
-    def construct_x_y(self,
-            df, add_time_in_day=True, add_day_in_week=False
-    ):
-        """
-        Generate samples from
-        :param df:
-        :param add_time_in_day:
-        :param add_day_in_week:
-        :return:
-        # x: (epoch_size, seq_len, num_nodes, input_dim)
-        # y: (epoch_size, horizon, num_nodes, output_dim)
-        """
-
-        num_samples, num_nodes = df.shape
-        data = np.expand_dims(df.values, axis=-1)
-        data_list = [data]
-
-        if add_time_in_day:
-            time_ind = (df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
-            time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
-            data_list.append(time_in_day)
-        if add_day_in_week:
-            day_in_week = np.zeros(shape=(num_samples, num_nodes, 7))
-            day_in_week[np.arange(num_samples), :, df.index.dayofweek] = 1
-            data_list.append(day_in_week)
-
-        x_offsets=np.arange(-self.seq_len+1,1)
-        y_offsets=np.arange(1,self.horizon+1)
-        data = np.concatenate(data_list, axis=-1)
-        # epoch_len = num_samples + min(x_offsets) - max(y_offsets)
-        x, y = [], []
-        # t is the index of the last observation.
-        min_t = abs(min(x_offsets))
-        max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
-        for t in range(min_t, max_t):
-            x_t = data[t + x_offsets, ...]
-            y_t = data[t + y_offsets, ...]
-            x.append(x_t)
-            y.append(y_t)
-        x = np.stack(x, axis=0)
-        y = np.stack(y, axis=0)
-        return x, y
-
-    def set(self,stage):
-        self.stage=stage
-        print("Now using %sing set"%(stage))
-        return stage
-
-    def get(self,batch_size):
-        self.current_batch=0
-        data=self.data[self.stage]
-        length=len(data)
-        batches=length/batch_size
-        def iterator():
-            while self.current_batch<batches:
-                idx=self.current_batch*self.batch_size
-                yield(data['x'][idx:idx+self.batch_size],data['y'][idx:idx+self.batch_size])
-                self.current_batch=self.current_batch+1
-        return iterator()
+def pickle_save(filename,object):
+    with open(filename,'wb') as f:
+        pickle.dump(object,f)
 
 class MyModel(nn.Module):
     def __init__(self):
         super(MyModel,self).__init__()
-
+        self.iuput_dim=2
+        self.output_dim=1
+        self.num_nodes=207
+        self.batch_sz=64
+        self.layer1=nn.Linear(self.iuput_dim*self.num_nodes*12,self.num_nodes*12)
     def forward(self,x):
         '''
         :param x: (batch_size,... other input dimensions)
         :return: y: (batch_size,... other output dimensions)
         '''
-        pass
+        x=x.reshape(self.batch_sz,self.iuput_dim*self.num_nodes*12)
+        x=self.layer1(x)
+        x=x.reshape(64,12,207,1)
+        return x
 
 class Process_Handler():
-    def __init__(self):
-        pass
+    def __init__(self,loader,dir_args,model_args,train_args):
+        self.loader=loader
+        self.save_dir=dir_args['save_dir']
+        self.log_dir=dir_args['log_dir']
+        self.batch_size=train_args['batch_size']
+        self.lr=train_args['learning_rate']
+        self.loss_fn=self.set_loss(train_args['loss_fn'])
+        self.model=MyModel()
+        if train_args['optimizer']=='Adam':
+            self.optimizer=optim.SGD(self.model.parameters(),lr=self.lr)
+
+    @staticmethod
+    def set_loss(loss_name):
+        if loss_name=='MSELoss':
+            return nn.MSELoss()
+        else:
+            raise AttributeError('No Such Loss')
 
     def train(self):
+        self.model.train()
+        self.loader.set('train')
+        for i,(x,y) in enumerate(self.loader.get(self.batch_size)):
+            x=torch.from_numpy(x).float()
+            y=torch.from_numpy(y).float()
+            pred=self.model(x)
+            loss=self.loss_fn(pred,y)
+            loss.backward()
+            self.optimizer.step()
         pass
 
     def val(self):
         pass
 
     def test(self):
+        self.model.eval()
+
         pass
 
-    def save(self):
-        pass
+    def save(self,filename):
+        torch.save({'model_state_dict': self.model.state_dict(),
+                    'optimizer_state_dict': self.optmz.state_dict()},
+                   filename)
+        return filename
 
-    def load(self):
-        pass
+    def load(self,filename):
+        ckp = torch.load(filename)
+        self.model.load_state_dict(ckp['model_state_dict'])
+        self.optmz.load_state_dict(ckp['optimizer_state_dict'])
+        return filename
 
 def main(args):
     dir_args=args['dir']
